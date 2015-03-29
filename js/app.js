b7e5cf93ce341930fdbcaaf5b4a00415c89ca9e6
dhananjay-weds-vanishree <vani.jay26@gmail.com>
2015-03-29 09:49:22 +0530

    ALL PAGES ARE DONE

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 	$('.bg-img').height($(document).height());
 
 	$(window).resize(function(){
-        console.log('in-resize');
+        // console.log('in-resize');
 		$('.bg-img').width($(window).width());
 		$('.bg-img').height($(window).height());
 	});
@@ -12,17 +12,48 @@ $(document).ready(function(){
 });
 
 var highlightLink = function(){
+
     var path = window.location.pathname;
-    console.log(path);
 
-    $('.resp-menu ul li').removeClass('active-url');
-
-    // // if('/^\/about-us.html/'.test(path)){
-    // // if(/^\/samprathi/.test(path)){
+    $('.nav-menus a').removeClass('active-url');
         if (path.indexOf('index.html') > 0)
             $('[data-url=home]').addClass('active-url');
         else if(path.indexOf('where.html') > 0)
             $('[data-url=where]').addClass('active-url');
         else if(path.indexOf('contact.html') > 0)
             $('[data-url=contact]').addClass('active-url');
-    };
+};
+
+
+var showRemainingTime = function(){
+
+    var end = new Date('04/24/2015 05:00 PM');
+    var _second = 1000;
+    var _minute = _second * 60;
+    var _hour = _minute * 60;
+    var _day = _hour * 24;
+    var timer;
+
+        function showRemaining() {
+            var now = new Date();
+            var diffrence = end - now;
+
+            if (diffrence < 0) {
+                clearInterval(timer);
+                document.getElementById('countdown').innerHTML = 'MAHURATAM OVER!';
+                return;
+            }
+
+            var days = Math.floor(diffrence / _day);
+            var hours = Math.floor((diffrence % _day) / _hour);
+            var minutes = Math.floor((diffrence % _hour) / _minute);
+            var seconds = Math.floor((diffrence % _minute) / _second);
+
+            document.getElementById('countdown').innerHTML = days + ' Days : ';
+            document.getElementById('countdown').innerHTML += hours + ' Hours : ';
+            document.getElementById('countdown').innerHTML += minutes + ' Mins : ';
+            document.getElementById('countdown').innerHTML += seconds + ' Secs';
+        }
+
+    timer = setInterval(showRemaining, 1000);
+};
